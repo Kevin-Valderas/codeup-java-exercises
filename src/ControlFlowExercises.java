289d061198b4b7eef8fc9186f2bbf216ea91d0ad
Kevin Valderas <kevin.d.valderas34@gmail.com>
2021-09-07 14:40:10 -0500

    completed ex 3 (fixed!)

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,4 +1,8 @@
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableModel;
 import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.function.Function;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
@@ -18,7 +22,7 @@ public class ControlFlowExercises {
 //        i = i + 2;
 //    } while(i <= 100);
 //
-    /*1.b.2 */
+        /*1.b.2 */
 //        int i = 100;
 //        do {
 //            System.out.print(i + "\n");
@@ -33,19 +37,46 @@ public class ControlFlowExercises {
 //        }while(i <= 100000);
 //
         /*2.0.0*/
-        for (int i = 1; i <= 100; i += 1) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            }else
-            if (i % 3 == 0) {
-                System.out.println("Fizz");
-            }else
-            if (i % 5 == 0) {
-                System.out.println("Buzz");
+//        for (int i = 1; i <= 100; i += 1) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("FizzBuzz");
+//            }else
+//            if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            }else
+//            if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            }
+//            else {
+//                System.out.println(i);
+//            }
+//        }
+
+        /*3.0.0*/
+        Scanner integer = new Scanner(System.in);
+        Scanner response = new Scanner(System.in);
+        boolean confirm;
+        do{
+        System.out.print("What number would you like to go up to? " );
+        int userInput = integer.nextInt();
+        String tableStart = """
+                Here is your table!
+
+                number | squared | cubed
+                ------ | ------- | -----
+                """;
+        System.out.println(tableStart);
+        for (int i = 1; i <= userInput; i++) {
+            int square = (int) Math.pow(i, 2);
+            int cube = (int) Math.pow(i, 3);
+            System.out.print(i + "      | " + square + "       | " + cube + "\n");
             }
-            else {
-                System.out.println(i);
-            }
+        System.out.println("Do you want to go again? ");
+        String userResponse = response.next();
+        if(userResponse.equalsIgnoreCase("yes") || userResponse.equalsIgnoreCase("y")){
+            confirm = true;
+        } else {
+            confirm = false;
         }
-    }
-}
+        }while(confirm);
+    }}
