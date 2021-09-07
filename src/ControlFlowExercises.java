335587244bad68525cb6503050c2cf7c1fb0745e
Kevin Valderas <kevin.d.valderas34@gmail.com>
2021-09-07 11:29:26 -0500

    completed ex. 1.a - 1.b.2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,26 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.print(i);
-            i++;
-        }
+        /*1.a*/
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.print(i);
+//            System.out.print("%n");
+//            i++;
+//        }
+
+        /*1.b.1*/
+
+//        int i = 2;
+//    do {
+//        System.out.print(i + "\n");
+//        i = i + 2;
+//    } while(i <= 100);
+//
+    /*1.b.2 */
+        int i = 100;
+        do {
+            System.out.print(i + "\n");
+            i = i - 5;
+        }while(i >= -10);
     }
 }
