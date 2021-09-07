b7a859ff0290be54c3359c8e8ff68751f7ba4772
Kevin Valderas <kevin.d.valderas34@gmail.com>
2021-09-07 11:55:55 -0500

    completed ex 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -33,7 +33,19 @@ public class ControlFlowExercises {
 //        }while(i <= 100000);
 //
         /*2.0.0*/
-
-
+        for (int i = 1; i <= 100; i += 1) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }else
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }else
+            if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
     }
 }
