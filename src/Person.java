fcfb9b368f80c582848bfb79eeaa04c20a6939e3
Kevin Valderas <kevin.d.valderas34@gmail.com>
2021-09-14 10:43:43 -0500

    Person done

--- src/Person.java
+++ src/Person.java
@@ -5,17 +5,11 @@ class Person {
         this.name = name;
     }
 
-    public String sayHello() {
-        return String.format("Hello from %s!", name);
-    }
     public String getName(){
         return name;
     }
-
-
-    public static void main(String[] args) {
-        Person person = new Person("John");
-        System.out.println(person.sayHello());
+    public void setName(String name){
+        this.name = name;
     }
 }
 
